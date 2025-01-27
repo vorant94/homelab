39c7894747539bb9af16c9e2e56e73189f7306dd
vorant94 <vorant94@pm.me>
2025-01-27 21:41:21 +0200

    end of the day setup

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # Start from scratch
 
 1. Install Raspberry Pi OS Lite on a MicroSD with [Raspberry Pi Imager](https://www.raspberrypi.com/software/)
-   - Raspberry Pi Imager is also available on [Homebrew](https://formulae.brew.sh/cask/raspberry-pi-imager)
-   - enable access via SSH during installation
+  - Raspberry Pi Imager is also available on [Homebrew](https://formulae.brew.sh/cask/raspberry-pi-imager)
+  - enable access via SSH during installation
 2. SSH into Raspberry Pi
 3. Install [Nix Package Manager](https://nixos.org/download/#download-nix)
-   - go with multi-user installation
-   - run `nix-channel --update` to fix `warning: Nix search path entry '/home/vorant94/.nix-defexpr/channels' does not exist, ignoring`, [source](https://github.com/NixOS/nixpkgs/issues/40165)
+  - go with multi-user installation
+  - run `nix-channel --update` to fix `warning: Nix search path entry '/home/vorant94/.nix-defexpr/channels' does not exist, ignoring`, [source](https://github.com/NixOS/nixpkgs/issues/40165)
 4. Install [Home Manager](https://nix-community.github.io/home-manager/index.xhtml#sec-install-standalone)
-   - choose latest stable channel (like 24.11)
+  - choose latest stable channel (like 24.11)
+5. Install `git` via Nix
+  - edit `~/.config/home-manager/home.nix` directly
+6. Clone Homelab [repo](https://github.com/vorant94/homelab)
+7. Replace default Home Manager folder with symlink to one in repo
+  - example `ln -s ~/homelab/home-manager ~/.config/home-manager`
 
 # Cheatsheet
 
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,85 +1,49 @@
 { config, pkgs, ... }:
 
 let
-  nanorcContent = import ./nanorc.nix { inherit pkgs; };
+  nanorc = import ./nanorc.nix { inherit pkgs; };
 in {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "vorant94";
-  home.homeDirectory = "/home/vorant94";
+  home = {
+    username = "vorant94";
+    homeDirectory = "/home/vorant94";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+    stateVersion = "24.11";
 
-  home.enableNixpkgsReleaseCheck = false;
+    enableNixpkgsReleaseCheck = false;
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    packages = [
+      pkgs.nano
+      pkgs.nanorc
+      pkgs.tmux
+      pkgs.git
+      pkgs.fzf
+    ];
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    file = {
+      ".nanorc".text = nanorc;
+    };
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-    pkgs.nano
-    pkgs.nanorc
-    pkgs.tmux
-    pggs.git
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-    ".nanorc".text = nanorcContent;
+    sessionVariables = {
+      EDITOR = "nano";
+    };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/vorant94/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nano";
-  };
+  programs = {
+    home-manager = {
+      enable = true;
+    };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+    git = {
+      enable = true;
+      
+      userName = "vorant94";
+      userEmail = "vorant94@pm.me";
+
+      extraConfig = {
+        init.defaultBranch = "master";
+        push.autoSetupRemote = true;
+        core.editor = "nano";
+      };
+    };
+  };
 }
