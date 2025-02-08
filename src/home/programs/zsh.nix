343cf0373814d0af36a661dc552840a07c85e402
vorant94 <vorant94@pm.me>
2025-02-08 19:40:20 +0000

    remove aliases since they are sourced from omz

--- src/home/programs/zsh.nix
+++ src/home/programs/zsh.nix
@@ -8,13 +8,6 @@
     FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
   };
 
-  shellAliases = {
-    la = "ls -lAFh";
-    c = "clear";
-    r = "source ~/.zshrc";
-    ".." = "cd ..";
-  };
-
   oh-my-zsh.enable = true;
   oh-my-zsh.theme = "dracula";
   oh-my-zsh.custom = "$HOME/.config/oh-my-zsh";
